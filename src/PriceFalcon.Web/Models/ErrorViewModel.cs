8603aaec076bde75c5885aedaaa18153c4aa3e22
Eliot Jones <elioty@hotmail.co.uk>
2021-03-17 15:28:48 -0400

    create solution and web projects

---
+++ src/PriceFalcon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PriceFalcon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
